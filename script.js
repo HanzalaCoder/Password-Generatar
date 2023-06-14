1a41ca5b108be149a7cc1d8080866883d1f8349d
Hanzala Khan <hanzalacoder061@gmail.com>
2023-06-14 23:05:32 +0500

    project complete

--- script.js
+++ script.js
@@ -17,9 +17,12 @@ const LowerCase = document.getElementById("lowercase")
 const Numberss = document.getElementById("numbers")
 const Symbols = document.getElementById("symbol")
 
+/* rank password  */
+const textrank = document.querySelector(".text-strength")
+const textrankimg = document.querySelectorAll(".bg")
+
 /* form */
 const form = document.querySelector(".form")
-
 
 Removeallcheckmark()
 setRangeValuetoshow()
@@ -30,35 +33,62 @@ rangebar.addEventListener("click",increseORdecreaserangeValue)
 imgcontainer.forEach((singlecontainer)=> {
     addTrurorFalsetocheckbox(singlecontainer)
 })
-form.addEventListener("submit",main)
 
+form.addEventListener("submit",main)
 
 /* variables */
 let upperLetters = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
 let lowerLetters = [...'abcdefghijklmnopqrstuvwxyz'];
 let allnumbers = [...'0123456789'];
-let allsymbol = [...'~!@#$%^&*_-+=']
-
-
+let allsymbol = [...'~!@#$%^&*_-+='];
+let MakePassaword = "";
+let passwordarray = [];
 
 function main(e) {
     e.preventDefault()
     reset()
-    length =  rangeNumber.innerText
-    if(UpperCase.classList.contains("true") && (LowerCase.classList.contains("false")))  {
-        console.log('1')
-    } else {
-        console.log("2")
+    lengthpwd =  rangeNumber.innerText
+    for (let i  = 0; i < lengthpwd;i++) {
+        if (UpperCase.classList.contains("true"))  {
+        let value = Math.floor(Math.random() * upperLetters.length )
+        value = upperLetters[value]
+        passwordarray.push(value)
+        let complete = limitthelenght(passwordarray)
+        if(complete) {
+            break
+        }} 
+        if (LowerCase.classList.contains("true"))  {
+        let value = Math.floor(Math.random() * lowerLetters.length )
+        value = lowerLetters[value]
+        passwordarray.push(value)
+        let complete = limitthelenght(passwordarray)
+        if(complete) {
+            break
+        }} 
+
+        if (Numberss.classList.contains("true"))  {
+        let value = Math.floor(Math.random() * allnumbers.length )
+        value = allnumbers[value]
+        passwordarray.push(value)
+        let complete = limitthelenght(passwordarray)
+        if(complete) {
+            break
+        } } 
+        if (Symbols.classList.contains("true"))  {
+        let value = Math.floor(Math.random() * allsymbol.length)
+        value = allsymbol[value]
+        passwordarray.push(value)
+        let complete = limitthelenght(passwordarray)
+        if(complete) {
+            break
+        }} 
     }
-
-   
-    
-
-   
+    passwordarray = shuffleArray(passwordarray)
+    MakePassaword = passwordarray.join("")
+    console.log(passwordarray)
+    rankComplexityandlength(MakePassaword,passwordarray)
+    password.value = MakePassaword
 }
-
-
-
 /* to increase the range value according to user choice */
 function increseORdecreaserangeValue() {
     let value = rangebar.value
@@ -78,6 +108,9 @@ function copytoClipboard() {
     password.setSelectionRange(0,99999)
     navigator.clipboard.writeText(password.value)
     copyDonemsg.classList.add("clipboard-msg-active")
+    setTimeout(() => {
+        copyDonemsg.classList.remove("clipboard-msg-active")
+    },3000)
 }
 
 /* to remove all added checkmark through html */
@@ -113,4 +146,86 @@ function addTrurorFalsetocheckbox(container) {
 function reset() {
     password.value = ""
     copyDonemsg.classList.remove("clipboard-msg-active")
+    password.value = ""
+    passwordarray = []
+    removeRankclasses()
+}
+
+/* to limit the length of password to given number by user number */
+function limitthelenght(array) {
+    if (array.length >= rangeNumber.innerText) {
+        return true
+    }
+}
+
+/* copy from chatgpt */
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array
+  }
+
+function rankComplexityandlength(pwdstring,pwdarray) {
+    
+    let score = 0
+    let regexUpperCase = /[A-Z]/;
+    let regexLowerCase = /[a-z]/;
+    let regexnumber = /\d+/g
+    let  symbolRegex = /[!@#$%^&*()_+\-=[\]{}|\\:;"'<>,.?/]/;
+    let foundUpper = pwdstring.match(regexUpperCase)
+    let foundlower = pwdstring.match(regexLowerCase)
+    let foundnum = pwdstring.match(regexnumber)
+    let foundsymbol = pwdstring.match(symbolRegex)
+    if (foundUpper !== null && foundUpper.length >= 1) {
+        score++;
+      }
+    if (foundlower !== null && foundlower.length >= 1) {
+        score++;
+      }
+    if (foundnum !== null && foundnum.length >= 1) {
+        score++;
+    }
+    if (foundsymbol !== null && foundsymbol.length >= 1) {
+        score++;
+    }
+
+    if (pwdarray.length < 6) {
+        score--
+    }
+
+    if (pwdarray.length >= 8) {
+        score++
+    }
+
+    if(score === 1) {
+        textrankimg[0].classList.add("bg-level1")
+        textrank.textContent = "To weak"
+    }
+    if(score === 2 || score === 3) {
+        textrankimg[0].classList.add("bg-level2")
+        textrankimg[1].classList.add("bg-level2")
+        textrank.textContent = "weak"
+    }
+    if(score=== 4) {
+        textrankimg[0].classList.add("bg-level3")
+        textrankimg[1].classList.add("bg-level3")
+        textrankimg[2].classList.add("bg-level3")
+        textrank.textContent = "medium"
+    }
+    if(score=== 5) {
+        textrankimg[0].classList.add("bg-level4")
+        textrankimg[1].classList.add("bg-level4")
+        textrankimg[2].classList.add("bg-level4")
+        textrankimg[3].classList.add("bg-level4")
+        textrank.textContent = "strong"
+    }
+}
+
+
+function removeRankclasses() {
+    textrankimg.forEach((singlecontainer)=> {
+        singlecontainer.classList.remove("bg-level1","bg-level2","bg-level3","bg-level4")
+    })
 }
